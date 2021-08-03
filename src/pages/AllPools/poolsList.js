0355f5234f6c1b3cd96f0d4c0e59f2b5e16ee253
mohammadrezaii <mmdrezaii825401530@gmail.com>
2021-08-03 12:12:08 +0430

    go to main branch

--- src/pages/AllPools/poolsList.js
+++ src/pages/AllPools/poolsList.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Tab } from "@headlessui/react";
 import ProgressBar from "../../components/layout/progressBar";
-import { Link } from "react-router-dom";
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { Link as Links } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function PoolsList() {
+
   useEffect(() => {
-    Aos.init({duration: 500})
-  }, [])
+    Aos.init({
+      duration: 500
+    });
+  }, []);
 
   let [categories] = useState({
     Ongoing: [
@@ -171,73 +174,72 @@ export default function PoolsList() {
         <Tab.Panels className="mt-5 md:mt-2 focus:outline-none focus:border-0">
           {Object.values(categories).map((posts, idx) => (
             <Tab.Panel key={idx} className="focus:outline-none focus:border-0">
-                <ul className="focus:outline-none focus:border-0">
-                  {posts.map((post) => (
-
-                      <li
-                          data-aos="fade-up"
-                          data-aos-once="true"
-                          key={post.id}
-                          className="focus:outline-none md:grid md:grid-cols-10 md:gap-x-2 cursor-pointer bg-gradient-to-b from-darkMode-800 to-darkMode-600 border border-gray-600 px-7 py-4 rounded-lg mt-3"
-                      >
-                        <div className="col-span-3 flex items-center space-x-4">
-                          <img
-                              className="rounded-full h-12"
-                              src={post.img}
-                              alt=""
-                          />
-                          <h3 className="text-base font-bold leading-5 text-white">
-                            {post.title}
-                          </h3>
-                        </div>
-                        <div className="flex md:hidden lg:flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
+              <ul className="focus:outline-none focus:border-0">
+                {posts.map((post) => (
+                  <li
+                    data-aos="fade-up"
+                    data-aos-once="true"
+                    key={post.id}
+                    className="focus:outline-none md:grid md:grid-cols-10 md:gap-x-2 cursor-pointer bg-gradient-to-b from-darkMode-800 to-darkMode-600 border border-gray-600 px-7 py-4 rounded-lg mt-3"
+                  >
+                    <div className="col-span-3 flex items-center space-x-4">
+                      <img
+                        className="rounded-full h-12"
+                        src={post.img}
+                        alt=""
+                      />
+                      <h3 className="text-base font-bold leading-5 text-white">
+                        {post.title}
+                      </h3>
+                    </div>
+                    <div className="flex md:hidden lg:flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
                       <span className="md:hidden font-semibold text-white text-opacity-90">
                         Status
                       </span>
-                          <span className="bg-gradient-to-t from-orange-500 to-orange-300 rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-xs leading-none">
+                      <span className="bg-gradient-to-t from-orange-500 to-orange-300 rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-xs leading-none">
                         Ongoing
                       </span>
-                        </div>
-                        <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
+                    </div>
+                    <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
                       <span className="md:hidden font-semibold text-white text-opacity-90">
                         Access
                       </span>
-                          <span className="rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-base leading-none">
+                      <span className="rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-base leading-none">
                         Private
                       </span>
-                        </div>
-                        <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
+                    </div>
+                    <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
                       <span className="md:hidden font-semibold text-white text-opacity-90">
                         Ratio, 1BUSD
                       </span>
-                          <span className="rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-base leading-none">
+                      <span className="rounded-full px-2.5 py-1.5 text-white text-center inline-block w-min font-semibold text-base leading-none">
                         8303.00
                       </span>
-                        </div>
-                        <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
+                    </div>
+                    <div className="flex justify-between mt-4 md:mt-0 md:self-center md:mx-auto">
                       <span className="md:hidden font-semibold text-white text-opacity-90">
                         Total raise
                       </span>
-                          <span className="rounded-full px-2.5 py-1.5 text-orange-500 text-center inline-block w-min font-semibold text-base leading-none">
+                      <span className="rounded-full px-2.5 py-1.5 text-orange-500 text-center inline-block w-min font-semibold text-base leading-none">
                         200K
                       </span>
-                        </div>
+                    </div>
 
-                        <div className="mt-6 mb-4 md:mt-0 md:mb-0 col-span-2 self-center">
-                          <ProgressBar progress={post.progress} display={"block"} />
-                        </div>
+                    <div className="mt-6 mb-4 md:mt-0 md:mb-0 col-span-2 self-center">
+                      <ProgressBar progress={post.progress} display={"block"} />
+                    </div>
 
-                        <div className="hidden md:col-start-10 md:flex justify-end items-center">
-                          <Link
-                              className="text-orange-500 border border-orange-500 rounded-xl leading-none px-4 py-2.5"
-                              to={"/"}
-                          >
-                            Apply
-                          </Link>
-                        </div>
-                      </li>
-                  ))}
-                </ul>
+                    <div className="hidden md:col-start-10 md:flex justify-end items-center">
+                      <Links
+                        className="text-orange-500 border border-orange-500 rounded-xl leading-none px-4 py-2.5"
+                        to={"/"}
+                      >
+                        Apply
+                      </Links>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </Tab.Panel>
           ))}
         </Tab.Panels>
