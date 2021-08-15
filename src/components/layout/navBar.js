bbe31bf1d7d63c7e79014030800beec8bc3fd0fa
mohammadrezaii <mmdrezaii825401530@gmail.com>
2021-08-15 16:28:35 +0430

    fix rocket rising problem on connect wallet modal

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -37,7 +37,7 @@ const NavBar = ({ darkMode, navigation }) => {
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
+          className="fixed inset-0 z-50 overflow-y-auto"
           onClose={closeModal}
         >
           <div className="min-h-screen px-4 text-center">
@@ -156,7 +156,7 @@ const NavBar = ({ darkMode, navigation }) => {
       <Transition appear show={isShow} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
+          className="fixed inset-0 z-50 overflow-y-auto"
           onClose={removeModal}
         >
           <div className="min-h-screen px-4 text-center">
