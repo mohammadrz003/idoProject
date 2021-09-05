a6d67bba00dd9e2304ea9728ff6dc78bd3a758c3
mohammadrezaii <rezaiimohammad003@gmail.com>
2021-09-05 23:40:40 +0430

    add swiper slider to partners in desktop screen

--- src/pages/Home/partners.js
+++ src/pages/Home/partners.js
@@ -1,12 +1,33 @@
 import { PartnerList } from "./style/partnerList";
+import SwiperCore, { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
 
 const Partners = () => {
+  SwiperCore.use([Autoplay]);
+
   return (
-    <div className="dark:bg-darkMode-600 dark:text-white text-center pt-8 pb-20">
-      <div className="container mx-auto px-4 xl:px-28 flex flex-col md:flex-row md:justify-center items-center space-y-12 md:space-y-0 md:space-x-9">
-        {PartnerList.map((item) => {
-          return <span className="inline-block w-3/6 lg:w-1/6">{item}</span>;
-        })}
+    <div className="dark:bg-darkMode-600 dark:text-white text-center pt-12 pb-24">
+      <div className="container mx-auto px-4 xl:px-28">
+        <Swiper
+          loop={true}
+          autoplay={{
+              delay: 1200,
+              disableOnInteraction: false
+          }}
+          spaceBetween={50}
+          slidesPerView={5}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {PartnerList.map((item) => {
+            return (
+              <SwiperSlide className="flex items-center cursor-pointer">
+                {item}
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
