5fefd1938039d339fb49ee3ed570593df95d459d
mohammadrezaii <rezaiimohammad003@gmail.com>
2021-09-08 13:55:18 +0430

    change landing text

--- src/pages/Home/landingSection.js
+++ src/pages/Home/landingSection.js
@@ -43,20 +43,20 @@ const LandingSection = ({ darkMode, landingContainerWidth }) => {
 
           <NavBar darkMode={darkMode} navigation={navigation} />
 
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <main className="mt-14 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline dark:text-white dark:text-opacity-95">
-                  Best launchpad for
+                  The Gems <span className="inline text-primary-400">
+                  Launcher
+                  </span>
                 </span>{" "}
-                <span className="block text-primary-400 xl:inline">
-                  online business
-                </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 dark:text-white dark:text-opacity-75 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua
-                Egestas purus viverra
+                The BullLaunchPad multipurpose platform built on the blockchain
+                for early stage cryptocurrency projects to gain access to an
+                incubator while supporting and enabling the environment to
+                launch their token.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow" onClick={commingSoon}>
@@ -68,8 +68,11 @@ const LandingSection = ({ darkMode, landingContainerWidth }) => {
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3" onClick={warningNotif}>
-                  <Link to={"/pools"} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-secondery-300 bg-primary-400 dark:bg-darkMode-500 dark:text-white dark:text-opacity-90 md:py-4 md:text-lg md:px-10">
-                  View all pools
+                  <Link
+                    to={"/pools"}
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-secondery-300 bg-primary-400 dark:bg-darkMode-500 dark:text-white dark:text-opacity-90 md:py-4 md:text-lg md:px-10"
+                  >
+                    View all pools
                   </Link>
                 </div>
               </div>
